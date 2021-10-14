3356a80ac4e75231fecb398882fa4fcaab5c4eab
Pavel Lotashinski <pavel.lotashinski@intexsoft.by>
2021-10-14 12:35:01 +0300

    ITX2SKILLS-254 create fix

--- src/main/java/by/latashinsky/models/Constants.java
+++ src/main/java/by/latashinsky/models/Constants.java
@@ -4,5 +4,5 @@ public final class Constants {
     public final static String PATTERN_DOUBLE = "^[0-9]*[.]?[0-9]+$" ;
     public final static String PATTERN_INT = "[0-9]+" ;
     public final static String PATTERN_DATE = "^\\d{4}-\\d{2}-\\d{2}$" ;
-    public final static String PATH = "/home/INTEXSOFT/pavel.lotashinski/work/javaTopic/" ;
+    public final static String PATH = "/home/INTEXSOFT/pavel.lotashinski/work/java-topics/" ;
 }
